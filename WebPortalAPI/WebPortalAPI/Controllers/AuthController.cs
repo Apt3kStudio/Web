dc8dca27aadb3af7391e697cc2c1ce1af159136c
DESKTOP-0LHOCST\Diosc <dioscarr@gmail.com>
2018-12-20 23:46:04 -0500

    Authentication Controller #1

---
+++ WebPortalAPI/WebPortalAPI/Controllers/AuthController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace WebPortalAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost("token")]
+        public ActionResult GetToken()
+        {
+            #region  security key 
+            string securityKey = "super_long_security_key";
+            #endregion
+
+            #region Symetric security key
+            var symetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(securityKey));
+            #endregion
+
+            #region signing credential 
+            var signingCredentials = new SigningCredentials(symetricSecurityKey, SecurityAlgorithms.HmacSha256Signature);
+            #endregion
+
+            #region Claims 
+            var claims = new List<Claim>();
+            //claims.Add(new Claim(ClaimTypes.Role, "Admin"));
+            claims.Add(new Claim("Custom_Claim", "Custom_Claim_Name"));
+            claims.Add(new Claim(ClaimTypes.Role, "PhoneAppUser"));
+            #endregion
+
+            #region create token
+            var token = new JwtSecurityToken(
+                 issuer: "smesk.in",
+                 audience: "readers",
+                 expires: DateTime.Now.AddHours(1),
+                 signingCredentials: signingCredentials,
+                 claims: claims);
+            #endregion
+
+
+            return Ok(new JwtSecurityTokenHandler().WriteToken(token));
+
+            
+        }
+    }
+}
