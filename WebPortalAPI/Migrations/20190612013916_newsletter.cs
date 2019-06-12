7867f9b84707e3fa071e0ada52ab81407bedd302
Nelson Vasquez <nelsonvasquez03@gmail.com>
2019-06-11 21:42:42 -0400

    News Letter DB

---
+++ WebPortalAPI/Migrations/20190612013916_newsletter.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebPortalAPI.Migrations
+{
+    public partial class newsletter : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Newsletter_Subscriptions",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Email = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Newsletter_Subscriptions", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Newsletter_Subscriptions");
+        }
+    }
+}
